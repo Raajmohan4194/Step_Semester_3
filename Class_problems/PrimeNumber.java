056b21a10f00cece417e20efb3e8dabf4494d205
Raajmohan <129014927+Raajmohan4194@users.noreply.github.com>
2026-08-23 20:54:44 +0530

    Class problems session 1 uploaded

---
+++ Class_problems/PrimeNumber.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class PrimeNumber{
+public static void main(String[]args){
+Scanner sc=new Scanner(System.in);
+int number=sc.nextInt();
+boolean isPrime=true;
+for(int i=2;i<number;i++){
+if(number%i==0){
+isPrime=false;
+break;
+}
+}
+System.out.println("Is the number "+number+" a Prime number? "+isPrime);
+}
+}
