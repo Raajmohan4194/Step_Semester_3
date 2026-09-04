7e8ee785bdcb663fcdffd6794efe75aee6a43504
Raajmohan <129014927+Raajmohan4194@users.noreply.github.com>
2026-09-04 23:41:45 +0530

    Add files via upload
    Session-4 practice problems uploaded

---
+++ Practice problems/MaxProfit.java
@@ -0,0 +1,20 @@
+public class MaxProfit {
+
+    static int maxProfit(int[] prices) {
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            int profit = prices[i] - minPrice;
+            maxProfit = Math.max(maxProfit, profit);
+            minPrice = Math.min(minPrice, prices[i]);
+        }
+
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+        System.out.println(maxProfit(new int[]{7, 6, 4, 3, 1}));
+    }
+}
